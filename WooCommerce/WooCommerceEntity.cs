36551910fd8cdd83a4bb1ea8da1403952fcd7d1d
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2024-12-12 08:53:40 +0100

    add check if woo update is from correct shop instance

--- WooCommerce/WooCommerceEntity.cs
+++ WooCommerce/WooCommerceEntity.cs
@@ -1,9 +1,34 @@
-﻿using WooCommerceNET.Base;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using WooCommerceNET.Base;
 
 namespace WooCommerce.NET.WooCommerce
 {
+    public class HrefObject
+    {
+        [JsonProperty("href")]
+        public string Href { get; set; }
+    }
+
+    public class Links
+    {
+        [JsonProperty("self")] 
+        public List<HrefObject> Self { get; set; } = new ();
+
+        [JsonProperty("collection")]
+        public List<HrefObject> Collection { get; set; } = new();
+
+        [JsonProperty("customer")]
+        public List<HrefObject> Customer { get; set; } = new();
+
+        [JsonProperty("up")]
+        public List<HrefObject> Up { get; set; } = new();
+    }
     public class WooCommerceEntity : JsonObject
     {
         public virtual long? id { get; set; }
+
+        [JsonProperty("_links")]
+        public Links Links { get; set; }
     }
 }
